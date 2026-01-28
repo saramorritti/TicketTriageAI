10c5b0a043c8150eee90305a720853175ce442fb
Sara Morritti <sara.morritti@alveo.it>
2026-01-28 12:09:16 +0100

    IngestTicket setup

---
+++ TicketTriageAI.Functions/TicketTriageAI.Core/Validators/TicketIngestedRequestValidator.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TicketTriageAI.Core.Models;
+
+namespace TicketTriageAI.Core.Validators
+{
+    public sealed class TicketIngestedRequestValidator : AbstractValidator<TicketIngestedRequest>
+    {
+        public TicketIngestedRequestValidator()
+        {
+            RuleFor(x => x.MessageId).NotEmpty();
+            RuleFor(x => x.From).NotEmpty();
+            RuleFor(x => x.Subject).NotEmpty();
+            RuleFor(x => x.Body).NotEmpty();
+
+            RuleFor(x => x.ReceivedAt)
+                .Must(x => x != default)
+                .WithMessage("receivedAt is required");
+        }
+    }
+}
---
+++ TicketTriageAI.Functions/TicketTriageAI.Functions/Common/ApiMessages.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicketTriageAI.Functions.Common
+{
+    internal static class ApiMessages
+    {
+        public const string EmptyBody = "Request body is empty";
+        public const string InvalidPayload = "Invalid request payload";
+        public const string ValidationFailed = "Validation failed";
+        public const string Accepted = "Ticket accepted for processing";
+    }
+}
